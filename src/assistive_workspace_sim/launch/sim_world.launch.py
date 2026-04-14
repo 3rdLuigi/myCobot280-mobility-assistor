c51aa669a8921e594843568fb18f9f88338d2923
3rdLuigi <luisgaribayiii@gmail.com>
2026-04-13 20:49:36 -0700

    Added a simulation of workspace in gazebo The reason is because I don't have the robot in person so this is the best way to test the code. This will allow me to use test the code without access to the robot.

---
+++ src/assistive_workspace_sim/launch/sim_world.launch.py
@@ -0,0 +1,39 @@
+import os
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    #Locate packages
+    pkg_workspace = get_package_share_directory('assistive_workspace_sim')
+    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
+
+    urdf_file = os.path.join(pkg_workspace,'urdf','workspace.urdf.xacro')
+
+    robot_state_publisher_node = Node(
+        package = 'robot_state_publisher',
+        executable = 'robot_state_publisher',
+        parameters = [{'robot_description' : Command(['xacro ', urdf_file])}],
+        output = 'screen'
+    )
+    #Launch Gazebo Harmonic
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')),
+        launch_arguments = {'gz_args': '-r empty.sdf'}.items(),
+    )
+    #Spawn workspace into Gazebo
+    spawn_entity = Node(
+        package = 'ros_gz_sim',
+        executable = 'create',
+        arguments = ['-name', 'assistive_workspace','-topic', 'robot_description','-z','0.0'],
+        output = 'screen',
+    )
+
+    return LaunchDescription([
+        robot_state_publisher_node,
+        gazebo,
+        spawn_entity
+    ])
